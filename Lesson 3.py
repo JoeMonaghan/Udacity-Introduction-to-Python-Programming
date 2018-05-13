9e6c06345ec017538e2e9df31f2ec59c68b578d6
Joseph Monaghan <j.j.monaghansoftware@gmail.com>
2018-05-13 14:56:11 +0100

    Lesson 3 updated and countries added.

--- Lesson 3.py
+++ Lesson 3.py
@@ -42,6 +42,46 @@ def remove_duplicates(elements):
             my_set.append(element)
     return my_set
 
+
+squares = set()
+
+
+# todo: populate "squares" with the set of all of the integers less
+# than 2000 that are square numbers
+
+
+# Note: If you want to call the nearest_square function, you must define
+# the function on a line before you call it. Feel free to move this code up!
+def nearest_square(limit):
+    answer = 0
+    while (answer + 1) ** 2 < limit:
+        answer += 1
+    return answer ** 2
+
+def get_square_numbers(limit):
+
+    square = 0
+    i = 2
+    limit = nearest_square(limit)
+
+    while square <= limit:
+        square = nearest_square(i)
+        squares.add(square)
+        i += 1
+
+    print(squares)
+
+
+def get_squares(limit):
+    i = 1
+    squares = list()
+    while (i ** 2) <= limit:
+        squares.append(i ** 2)
+        i += 1
+
+    return squares
+
+
 def main():
 
     # test #REORGAINIZING CODE
@@ -55,10 +95,39 @@ def main():
     elements = [1, 33, 4, 4, 55, 55, 6, 3, 1, 9, 34]
     print(remove_duplicates(elements))
 
+    temp = dict(name='joe', age=25)
 
+    temp['home'] = 'IRE'
+
+    name = temp.get('name', default='Unknown name')
+
+    print(temp)
+
+    if 'home' in temp:
+        print('I have a home')
+        print(name)
+
+
+    squares = get_squares(25)
+    print(squares)
 
 if __name__ == '__main__':
     main()
+
+from countries import country_list # Note: since the list is so large, it's tidier
+                                   # to put in in a separate file. We'll learn more
+                                   # about "import" later on.
+
+country_counts = {}
+for country in country_list:
+    #todo: insert countries into the country_count dict.
+    # If the country isn't in the dict already, add it and set the value to 1
+    # If the country is in the dict, increment its value by one to keep count
+    if country_counts.get(country) is None:
+        country_counts[country] = 1
+    else:
+        country_counts[country] += 1
+
 
 
 
